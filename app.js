e8d0d0bec4a8c2cb267c695d847d433261e44c79
ideee007 <adebiyiidowuabdulhazim@gmail.com>
2024-09-11 13:48:05 +0100

    commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const alert = "Fire Alert! Fire Alert!! Fire Alert!!! Please use the exit closest to you"
+const interval = 3000 // 3 seconds
+
+var time = 0
+function setTime() {
+    time = time + 3;
+
+    console.log(alert);
+    if (time >= 30) {
+        clearInterval(timer);
+    }
+}
+timer = setInterval(setTime, interval)
